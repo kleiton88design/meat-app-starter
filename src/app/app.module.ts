8cfa9f00faf8220d0d66456411beab51e5272fb4
Kleiton Marques <kleiton.88design@gmail.com>
2019-01-27 13:47:38 -0200

    Criando componente de total de compra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,8 @@ import { InputComponent } from './shared/input/input.component';
 import { RadioComponent } from './shared/radio/radio.component';
 import { OrderItemsComponent } from './order/order-items/order-items.component';
 
-import {OrderService} from './order/order.service'
+import {OrderService} from './order/order.service';
+import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component'
 
 @NgModule({
   declarations: [
@@ -43,7 +44,8 @@ import {OrderService} from './order/order.service'
     OrderComponent,
     InputComponent,
     RadioComponent,
-    OrderItemsComponent
+    OrderItemsComponent,
+    DeliveryCostsComponent
   ],
   imports: [
     BrowserModule,
