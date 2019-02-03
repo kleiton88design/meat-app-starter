ac9c8ebfa0b4b1cb39a400e79e49ebcf77cd866b
Kleiton Marques <kleiton.88design@gmail.com>
2019-02-03 18:34:07 -0200

    navegacao programatica via Router

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router'
 import {RadioOption} from '../shared/radio/radio-option.model'
 import {OrderService} from './order.service'
 import {CartItem} from '../restaurant-detail/shopping-cart/cart-item.model'
@@ -18,7 +19,8 @@ export class OrderComponent implements OnInit {
 		{label: 'Cartão Refeição', value: 'REF'}
 	]
 
-	constructor(private orderService: OrderService) { }
+	constructor(private orderService: OrderService,
+				private router: Router) { }
 
 	ngOnInit() {
 	}
@@ -48,7 +50,7 @@ export class OrderComponent implements OnInit {
 			.map((item:CartItem)=>new OrderItem(item.quantity, item.menuItem.id))
 		this.orderService.checkOrder(order)
 		.subscribe( (orderId: string) => {
-			console.log(`Compra Concluída: ${orderId}`)
+			this.router.navigate(['/order-summary'])
 			this.orderService.clear()
 		})
 		console.log(order)
