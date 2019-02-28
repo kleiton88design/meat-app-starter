8bcd497d64706f8b58c95d39d252286d45c67c1c
Kleiton Marques <kleiton.88design@gmail.com>
2019-02-28 14:12:33 -0300

    Definindo um snackbar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -8,12 +8,13 @@ import {RatingComponent} from './rating/rating.component'
 
 import {OrderService} from '../order/order.service'
 import {ShoppingCartService} from '../restaurant-detail/shopping-cart/shopping-cart.service'
-import {RestaurantsService} from '../restaurants/restaurants.service'
+import {RestaurantsService} from '../restaurants/restaurants.service';
+import { SnackbarComponent } from './snackbar/snackbar.component'
 
 @NgModule({
-	declarations: [InputComponent, RadioComponent, RatingComponent],
+	declarations: [InputComponent, RadioComponent, RatingComponent, SnackbarComponent],
 	imports: [CommonModule, FormsModule, ReactiveFormsModule],
-	exports: [InputComponent, RadioComponent,
+	exports: [InputComponent, RadioComponent, SnackbarComponent,
 				RatingComponent, CommonModule,
 				FormsModule, ReactiveFormsModule]
 })
