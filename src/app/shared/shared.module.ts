6d89a4612cbec185706e29e946377eae523bdb11
Kleiton Marques <kleiton.88design@gmail.com>
2019-02-28 16:44:15 -0300

    Usando observable com snackbar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -9,7 +9,9 @@ import {RatingComponent} from './rating/rating.component'
 import {OrderService} from '../order/order.service'
 import {ShoppingCartService} from '../restaurant-detail/shopping-cart/shopping-cart.service'
 import {RestaurantsService} from '../restaurants/restaurants.service';
-import { SnackbarComponent } from './snackbar/snackbar.component'
+import { SnackbarComponent } from './messages/snackbar/snackbar.component'
+
+import {NotificationService} from './messages/notification.service'
 
 @NgModule({
 	declarations: [InputComponent, RadioComponent, RatingComponent, SnackbarComponent],
@@ -23,7 +25,7 @@ export class SharedModule {
 	static forRoot(): ModuleWithProviders {
 		return {
 			ngModule: SharedModule,
-			providers:[ShoppingCartService, RestaurantsService, OrderService]
+			providers:[ShoppingCartService, RestaurantsService, OrderService, NotificationService]
 		}
 	}
 }
