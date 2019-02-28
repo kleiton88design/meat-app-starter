5a2d29fa5e033e51a4a9f3477cfd1bed9e6dcb02
Kleiton Marques <kleiton.88design@gmail.com>
2019-02-28 19:20:47 -0300

    rotas com wildcard (Criando pagina nao encontrada)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,8 @@ import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
 
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 
-import {SharedModule} from './shared/shared.module'
+import {SharedModule} from './shared/shared.module';
+import { NotFoundComponent } from './not-found/not-found.component'
 
 @NgModule({
   declarations: [
@@ -34,7 +35,8 @@ import {SharedModule} from './shared/shared.module'
     ShoppingCartComponent,
     MenuItemComponent,
     ReviewsComponent,
-    OrderSummaryComponent
+    OrderSummaryComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
